21f22cb77684b2b3bd9e1c4e008c019ef8c41725
kirino <returnkings44@gmail.com>
2024-06-07 12:44:27 +0800

    ds

---
+++ javs/learnmore.js
@@ -0,0 +1,5 @@
+const navigation =
+    document.querySelector(".primary-navigation");
+const navigationHeight = 
+    navigation.offsetHeight;
+document.documentElement.style.setProperty("--scroll-padding",navigationHeight + "px");
